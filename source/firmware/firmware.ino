f0c6fac09973f37ead46ec6615c5dd36163fa402
Shadowino <savlovichdenis@gmail.com>
2022-08-30 19:40:51 +0300

    add esp01 Code

---
+++ source/firmware/firmware.ino
@@ -0,0 +1,19 @@
+/*
+  ver at 30.08.2022 for ESP01
+  TODO:
+  LED indicator controller
+  Buzzer dinamyc controller
+  Servo (motor modification) controller
+  wifi in STA mode with "welcome page"
+ */
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+
+  
+}
